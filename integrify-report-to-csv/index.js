b95e76b64705095bcc24172431ba56f61c400a65
Rich Trusky <rtrusky@integrify.com>
2017-10-06 12:04:04 -0400

    Adding report-to-csv

--- integrify-report-to-csv/index.js
+++ integrify-report-to-csv/index.js
@@ -7,7 +7,7 @@ var path = require('path');
 //create a new Integrfiy AWS Lambda object passing in a configuration object with inputs, outputs and your execute function
 var config = {
         inputs: [{key:"reportSid", type:"string"},
-            {key:"fileName", type:"string"}, {key:"fileExtension", type:"string"}],
+            {key:"csvFileName", type:"string"}],
         outputs:[{key:"fileKey", type:"fileattachment"}, {key:"fileName", type: "string"}]
 }
 
@@ -28,12 +28,18 @@ var exec = function (event, context, callback) {
             console.error(err);
             return callback(err);
         }
-        filters = JSON.parse(filters);
+        let parsedFilters = [];
+        try {
+            parsedFilters = JSON.parse(filters);
+        } catch(e){
+            console.log("no filters")
+        }
+
 
 ///[{"MappingVal":"Request|ID","Expose":"ID","Value":null,"Text":null,"Operator":"Is","FieldType":"textfield","Options":null}]
         try {
             //loop though filters and populate with inputs
-            filters.forEach(f=>{
+            parsedFilters.forEach(f=>{
                 f.Value = event.inputs[f.Expose] || "";
             })
 
@@ -55,16 +61,32 @@ var exec = function (event, context, callback) {
 
         let reportRunUrl = `${integrifyServiceUrl}/reports/${event.inputs.reportSid}/tofile/csv?filters=${filters}`;
 
+
         request.get({url: reportRunUrl,
 
             'auth': {
                 'bearer': event.inputs.accessToken || event.accessToken
-            }}).pipe(
-                request.post(request.post(integrifyServiceUrl + '/files/upload/', {'auth': {
-                    'bearer': event.inputs.accessToken || event.accessToken
-                }}))
-            ).on('response', function(response){
-                console.log(response.statusCode);
+            }},function(err1,rsp1,reportData){
+
+            if (err1) return callback(err1);
+
+            var req = request.post(integrifyServiceUrl + '/files/upload/', {'auth': {
+                'bearer': event.inputs.accessToken || event.accessToken
+            }}, function (err, httpResponse, fileInfo) {
+                if (err) {
+                    console.error('upload failed:', err);
+                    return callback(err);
+                }
+                console.log('Upload successful!  Server responded with:', fileInfo);
+                let fileKey = JSON.parse(fileInfo)[0].file;
+                return callback(null, {fileKey: fileKey, fileName: event.inputs.fileName});
+
+            });
+            var form = req.form();
+            form.append('file', reportData, {
+                filename: event.inputs.csvFileName.indexOf(".") == -1 ? event.inputs.csvFileName + ".csv" : event.inputs.csvFileName,
+                contentType: "text/csv"
+            });
         })
     })
 
