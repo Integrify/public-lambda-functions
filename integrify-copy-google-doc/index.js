cda619e501daceb1104db396913f0c73e262af0c
Rich Trusky <rtrusky@integrify.com>
2018-03-13 16:58:01 -0400

    D - added function to copy google drive file

--- integrify-copy-google-doc/index.js
+++ integrify-copy-google-doc/index.js
@@ -1,34 +1,76 @@
 "use strict";
 var integrifyLambda = require('integrify-aws-lambda');
-var request = require("request");
+let google = require('googleapis');
+let privatekey = require("./privatekey.json");
+let jwtClient = new google.auth.JWT(
+    privatekey.client_email,
+    null,
+    privatekey.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/calendar']);
+//authenticate request
+jwtClient.authorize(function (err, tokens) {
+    if (err) {
+        console.log(err);
+        return;
+    } else {
+        console.log("Successfully connected!");
+
+    }
+});
+
+let sheets = google.sheets('v4');
+let drive  = google.drive('v3');
+
+
 
 //create a new Integrfiy AWS Lambda object passing in a configuration object with inputs, outputs and your execute function
-var slack = new integrifyLambda({
+var gcopy = new integrifyLambda({
+        helpUrl: "http://www.integrify.com",
         inputs: [{key:"fileId", type:"string"},
             {key:"newFileName", type:"string"},
             {key:"newTitle", type:"string"},
-            {key:"requestName", type:"string"},
-            {key: "anyoneWithLinkRole", type:"string"},
-            {key: "includeLink", type:"string"}],
-        outputs:[{key:"id", type:"string"},{key:"mimeType", type:"string"},{key:"name", type:"string"},{key:"url", type:"string"}, {key:"minimalUrl", type:"string"}],
+            {key: "anyoneWithLinkRole", type:"string"}],
+        outputs:[{key:"fileId", type:"string"},{key:"mimeType", type:"string"},{key:"name", type:"string"},{key:"url", type:"string"}, {key:"embedlUrl", type:"string"}],
         execute: (event, context, callback) => {
             console.info(event);
-            let messageBody = `Integrify Request ${event.inputs.requestId} - ${event.inputs.requestName} - ${event.inputs.requestStatus}
-${event.inputs.message}
-`
-            let message = {};
-           if (event.inputs.includeLink) {
-                message.text = messageBody + `<${event.integrifyServiceUrl}#/section-dashboard/request/${event.inputs.requestSid}|view Request ${event.inputs.requestId}>`
-            }
+            let inputs = event.inputs;
+            let resource =  { name: inputs.newFileName, title: inputs.newTitle}
+            drive.files.copy({auth: jwtClient,  fileId: inputs.fileId,  resource: resource}, function(err,resp2) {
+                if (err) {
+                    console.error(err);
+                    return callback(err);
+                }
+                let created = resp2.data;
+                let outputs = {
+                    fileId: created.id,
+                    mimeType: created.mimeType,
+                    name: created.name,
+                    url:  "https://drive.google.com/open?id=" + created.id,
+                    embedUrl:  "https://drive.google.com/open?minimal=true&id=" + created.id
+                }
 
-            request.post({url: event.inputs.webHookURL, json: true, body: message}, function(err, rsp, body){
-                if (err) return callback(err);
-                callback(null, {messageStatus: body})
-            })
+                drive.permissions.create({
+                    auth: jwtClient, fileId: created.id, resource: {
+                        value: null,
+                        type: "anyone",
+                        role: inputs.anyoneWithLinkRole
+                    }
+                }, function (err, perms) {
+                    if (err) {
+                        console.error(err);
+                        return callback(err);
+                    }
+                    return callback(null, outputs)
+
+
+                });
+            });
 
         }
 
 });
 
 //Export the handler function of the new object
-exports.handler = slack.handler;
+exports.handler = gcopy.handler;
--- integrify-copy-google-doc/test/CopyDocSpec.js
+++ integrify-copy-google-doc/test/CopyDocSpec.js
@@ -1,13 +1,45 @@
 "use strict";
 var slackLambda = require("../index.js");
 var expect = require("expect")
+let google = require('googleapis');
+let privatekey = require("../privatekey.json");
+let jwtClient = new google.auth.JWT(
+    privatekey.client_email,
+    null,
+    privatekey.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/calendar']);
+//authenticate request
+jwtClient.authorize(function (err, tokens) {
+    if (err) {
+        console.log(err);
+        return;
+    } else {
+        console.log("Successfully connected!");
 
+    }
+});
+let drive  = google.drive('v3');
+
+var created = "";
 
 //create an instance of the IntegrifyLambda with the config
 
 
 it("should return config.inputs", function() {
     var event = {"operation": "config.getInputs"}
+    slackLambda.handler(event, null, function(err,result){
+        "use strict";
+        //console.log(result)
+        expect(result.length).toBeGreaterThan(0);
+
+    })
+
+});
+
+it("should return config.help", function() {
+    var event = {"operation": "config.getHelpUrl"}
     slackLambda.handler(event, null, function(err,result){
         "use strict";
         //console.log(result)
@@ -28,19 +60,14 @@ it("should return config.outputs", function() {
 
 });
 
-
-
 it("should execute and return values", function(done) {
+    this.timeout(100000);
     var event = { "operation": "runtime.execute",
-        "inputs":{
-            "webHookURL" : "https://hooks.slack.com/services/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",
-            "requestId":"1234",
-            "requestSid":"e1c6082d-62cb-46d0-9a77-374ef836d49d",
-            "requestName":"Fake Request",
-            "requestStatus":"Submitted",
-            "includeLink":"true",
-            "message":"this is a test"
-
+        inputs: {
+        fileId:"1tiOUy2rHSOIZn3S-pO7mE3dJvMpbKD8JWLUrAl4UjrA",
+        newFileName: "UnitTest-" + new Date().toISOString(),
+        newTitle: "UnitTest-" + new Date().toISOString(),
+        anyoneWithLinkRole: "writer",
         },"integrifyServiceUrl":"http://localhost:3000"
 
     }
@@ -48,9 +75,20 @@ it("should execute and return values", function(done) {
     slackLambda.handler(event, null, function(err,result){
         "use strict";
         //console.log(result)
-        expect(result.messageStatus).toExist();
+        expect(result.fileId).toExist();
+        created = result;
         done();
 
     })
 
 });
+
+it("should have set the permissions on the file to 'writer'", function(done){
+    this.timeout(100000);
+    drive.permissions.list({auth: jwtClient, fileId: created.fileId}, function (err, perms){
+        console.log(perms.data)
+        let perm = perms.data.permissions.find(p => p.id === 'anyoneWithLink')
+        expect(perm.role).toEqual('writer');
+        done();
+    });
+})
