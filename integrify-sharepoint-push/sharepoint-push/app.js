04c089a45d577fd5473f937bc5c1be12fef75ca6
rtrusky <rtrusky@integrify.com>
2021-12-06 13:45:46 -0500

    aws sharepoint-push lambda working

--- integrify-sharepoint-push/sharepoint-push/app.js
+++ integrify-sharepoint-push/sharepoint-push/app.js
@@ -4,6 +4,11 @@ import {spsave} from "spsave"
 
 import map from 'map-stream'
 import fetch from 'node-fetch';
+import buffStream from "vinyl-source-buffer"
+// import {Transform,Readable, Writable} from "stream";
+// const pass = new Transform();
+
+
 // //create a new Integrfiy AWS Lambda object passing in a configuration object with inputs, outputs and your execute function
 let config = {
     helpUrl: "http://www.integrify.com",
@@ -44,63 +49,61 @@ const handler = async (event, context) => {
         const filesResp = await fetch(filesEndpint, {method:'GET', headers: {'Authorization': 'Bearer ' + event.accessToken}})
         let integrifyFiles = await filesResp.json();
 
-            if (integrifyFiles.length == 0) {
-                console.error(err || "no files found" );
-                return "no files found";
-            }
+        if (integrifyFiles.length == 0) {
+            console.error(err || "no files found" );
+            return "no files found";
+        }
+
+    
+        let integrifyFile = integrifyFiles.sort(function(a,b) {
+            return new Date(a.createdDate).getTime() - new Date(b.createdDate).getTime()
+        }).find(f => f.fileName == event.inputs.file);
+
+        if (!integrifyFile) {
+            let em = "no matching file for this request";
+            console.error(em);
+            return em
+        }
+    
+        //get the file from Integrify and save it to sharepoint
+        let creds = {username: event.inputs.userName , password: event.inputs.password };
+        let siteUrl = event.inputs.sharePointUrl ;
+
+        let integrifyFileUrl = integrifyFile.downloadLink;
+        const x = await fetch(event.integrifyServiceUrl + integrifyFileUrl, {method: 'GET', headers: {'Authorization': 'Bearer ' + event.accessToken}});
+
+        let spSaveCoreOpts = {
+            siteUrl: event.inputs.sharePointUrl
+        }
+        spSaveCoreOpts.checkin = event.inputs.checkin || false;
+        if(spSaveCoreOpts.checkin) {
+            spSaveCoreOpts.checkinType = event.inputs.checkinType || 0;
+            spSaveCoreOpts.checkinMessage = event.inputs.checkinMessage || "uploaded from Integrify";
+        }
     
     
-            let integrifyFile = integrifyFiles.sort(function(a,b) {
-                return new Date(a.createdDate).getTime() - new Date(b.createdDate).getTime()
-            }).find(f => f.fileName == event.inputs.file);
     
-            if (!integrifyFile) {
-                let em = "no matching file for this request";
-                console.error(em);
-                return em
-            }
+        spSaveCoreOpts.checkin = event.inputs.checkin || false;
+        spSaveCoreOpts.checkin = event.inputs.checkin || false;
+
+        let file;
+        const spresult = await spsave(spSaveCoreOpts, {username: event.inputs.username, password: event.inputs.password}, {
+            fileContent: new Buffer(await x.arrayBuffer()),
+            fileName:event.inputs.file,
+            folder: event.inputs.destinationFolder
+        })
+        console.log(spresult);
+     
+        let spUrl = event.inputs.sharePointUrl + "/" + event.inputs.destinationFolder + "/" + event.inputs.file;
     
-            //get the file from Integrify and save it to sharepoint
-            let creds = {username: event.inputs.userName , password: event.inputs.password };
-            let siteUrl = event.inputs.sharePointUrl ;
+
+        const result = {sharePointFileUrl: spUrl}
     
-            let integrifyFileUrl = integrifyFile.downloadLink;
-            const x = await fetch(event.integrifyServiceUrl + integrifyFileUrl, {method: 'GET', headers: {'Authorization': 'Bearer ' + event.accessToken}})
-    
-            let spSaveCoreOpts = {
-                siteUrl: event.inputs.sharePointUrl
-            }
-            spSaveCoreOpts.checkin = event.inputs.checkin || false;
-            if(spSaveCoreOpts.checkin) {
-                spSaveCoreOpts.checkinType = event.inputs.checkinType || 0;
-                spSaveCoreOpts.checkinMessage = event.inputs.checkinMessage || "uploaded from Integrify";
-            }
-    
-    
-    
-            spSaveCoreOpts.checkin = event.inputs.checkin || false;
-            spSaveCoreOpts.checkin = event.inputs.checkin || false;
-            x.body.pipe(buffStream(event.inputs.file))
-                .pipe(map(function(file, done) {
-                    spsave(spSaveCoreOpts, {username: event.inputs.username, password: event.inputs.password}, {
-                        file: file,
-                        folder: event.inputs.destinationFolder
-                    }).then(function (x) {
-                        console.log(x);
-                        let spUrl = event.inputs.sharePointUrl + "/" + event.inputs.destinationFolder + "/" + event.inputs.file;
-                        return {sharePointFileUrl: spUrl};
-    
-                    }).catch(function (err) {
-                        console.error(err);
-                        callback(err);
-    
-                    }).finally(function(){
-                        return done();
-                    })
-                }));
-    
+        return result;
+               
+
+             
         
-    
         
     } catch (err) {
         console.log(err);
@@ -108,6 +111,7 @@ const handler = async (event, context) => {
     }
 
 };
+
 config.execute = handler;
 let lambdaTask = new integrifyLambda(config)
 const lambdaHandler = lambdaTask.handler;
--- integrify-sharepoint-push/sharepoint-push/tests/unit/SharePointPush.test.js
+++ integrify-sharepoint-push/sharepoint-push/tests/unit/SharePointPush.test.js
@@ -1,6 +1,7 @@
 import {lambdaHandler}  from "../../app.js";
 import {expect}  from  'chai';
 
+let accessToken =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250YWN0R3VpZCI6IjRFOTlFMERELUIzQjMtNEQ1RC04MjZELTNENUFFNjJBMTZDRiIsImNvbnRhY3RJZCI6MSwibnRJZCI6ImlBcHByb3ZlIiwiZmlyc3ROYW1lIjoiU3lzdGVtIiwibWlkZGxlTmFtZSI6ImJvYiIsImxhc3ROYW1lIjoiU3lzdGVtIiwiZW1haWwiOiJuby1yZXBseUBpbnRlZ3JpZnkuY29tIiwicGhvbmUiOm51bGwsImxvY2F0aW9uIjpudWxsLCJhZGRyZXNzMSI6bnVsbCwiYWRkcmVzczIiOm51bGwsImNpdHkiOm51bGwsInN0YXRlIjpudWxsLCJ6aXAiOm51bGwsImNvdW50cnkiOm51bGwsInRpdGxlIjpudWxsLCJkZXBhcnRtZW50IjpudWxsLCJjb3N0Q2VudGVyIjpudWxsLCJkaXZpc2lvbiI6bnVsbCwicmVwb3J0c1RvR3VpZCI6bnVsbCwic2lnbmF0dXJlR3VpZCI6bnVsbCwiY0ZpZWxkMSI6bnVsbCwiY0ZpZWxkMiI6bnVsbCwibGFzdFVwZGF0ZSI6bnVsbCwibGFuZ3VhZ2VHdWlkIjoiMkE4MDNEODAtREU2RC00QURDLUE2NDItNEUzRDgzMUY1NDYwIiwidGltZXpvbmUiOiJVVEMiLCJsb2NhbGUiOiJlbi1VUyIsImNyZWF0ZWREYXRlIjoiMjAwNi0wMS0wMVQwNTowMDowMC4wMDBaIiwiY3JlYXRlZEJ5IjoiNEU5OUUwREQtQjNCMy00RDVELTgyNkQtM0Q1QUU2MkExNkNGIiwibW9kaWZpZWREYXRlIjoiMjAyMS0xMC0zMVQxODowMDowOC42MjNaIiwibW9kaWZpZWRCeSI6IkQ4QzQ2OUMzLUY3ODEtNDE3OS1BMDlDLTZEMTQ4ODg1RkIzMCIsImRlbGV0ZWREYXRlIjpudWxsLCJkZWxldGVkQnkiOm51bGwsImNvbnRhY3RTaWQiOiI0RTk5RTBERC1CM0IzLTRENUQtODI2RC0zRDVBRTYyQTE2Q0YiLCJ1c2VyTmFtZSI6ImlBcHByb3ZlIiwicGFzc3dvcmRSZXNldERhdGUiOm51bGwsInRlbmFudCI6ImludGVncmlmeWRldiIsIm5hbWUiOiJTeXN0ZW0gU3lzdGVtIiwiaWF0IjoxNjM4ODAyNDcxLCJleHAiOjEwMDE2Mzg4MDI0NzAsImF1ZCI6ImludGVncmlmeWRldiJ9.J50xqLAhftsQM18VjjaFP_3jDpfg8lmDzKR7p34UvOo"
 
 //create an instance of the IntegrifyLambda with the config
 
@@ -37,27 +38,26 @@ it("should return config.outputs", function() {
 //     {key: "'metaData", type: "string"}],
 //     outputs:[{key:"sharePointFileUrl", type:"string"}
 
-it("should execute and return values", function(done) {
+it("should execute and return values", async function() {
     this.timeout(100000);
     var event = { "operation": "runtime.execute",
         "inputs":{
-            "requestSid": "33581110-7424-45d0-bd18-a4ee9df0d419",
-            "file" : "2014-04-16T19-00-17.386Z.json",
-            "sharePointUrl":"https://yourcompany.sharepoint.com",
-            "destinationFolder":"Shared Documents/Test",
-            "username": "blah",
-            "password" : "blah"
-        },"integrifyServiceUrl":"http://localhost:3500",
-        "accessToken":"20e22223855e4a5ab0e6c17e2b49d409"
+            "requestSid": "7187C132-C33C-4132-88CF-ACF3AEB5D9D3",
+            "file" : "example1.html",
+            "sharePointUrl":"https://integrify531.sharepoint.com/",
+            "destinationFolder":"Shared Documents/testing",
+            "username": "rich.trusky@integrify.com",
+            "password" : "5ysc6ZUdX5sxpQ"
+        },"integrifyServiceUrl":"http://integrifydev.localhost:8233",
+        "accessToken": accessToken
 
     }
 
-    lambdaHandler(event, null, function(err,result){
-        "use strict";
-        //console.log(result)
-        expect(result.sharePointFileUrl).to.exist
-        done();
+    let result = await lambdaHandler(event, null);
 
-    })
+        //console.log(result)
+    expect(result.sharePointFileUrl).to.exist
+   
+    
 
 });
